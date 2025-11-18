b5072a6f7bd3ecdf3610183ab2d6065146886950
Minemaster656 <glebbasmanov123@gmail.com>
2025-11-18 12:48:41 +0400

    можно заглянуть в таблицу!

--- main.py
+++ main.py
@@ -33,9 +33,53 @@ def tables():
             pass
         return Response(dumps({"error": str(e)}), status=500)
 
+
 @app.get("/api/databases")
 def databases():
     return Response(dumps(["db/" + x for x in os.listdir("db")], ensure_ascii=False), status=200)
-    
+
+
+@app.get("/api/tables/sample")
+def sample_table():
+    db = request.headers.get("db")
+    if not db:
+        return Response('{"error":"Укажите базу данных!"}', status=400)
+    if not os.path.exists(db):
+        return Response('{"error":"База данных не найдена"}', status=404)
+    conn = None
+    try:
+        table = request.headers.get("table")
+        if not table:
+            return Response('{"error":"Укажите таблицу!"}', status=400)
+        conn = connect(db)
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = [row[0] for row in cur.fetchall()]
+        if table not in tables:
+            return Response('{"error":"Таблица в базе данных не найдена"}', status=404)
+        cur.execute(f"SELECT * FROM {table} LIMIT 256")
+        rows = cur.fetchall()
+        columns = [desc[0] for desc in cur.description]
+        # column_types = [desc[1] for desc in cur.description]
+        # print(cur.description)
+        data = []
+        for row in rows:
+            row_dict = {}
+            for i, value in enumerate(row):
+                if isinstance(value, bytes):
+                    row_dict[columns[i]] = value.decode(
+                        'utf-8', errors='replace')
+                else:
+                    row_dict[columns[i]] = value
+            data.append(row_dict)
+
+        # column_info = {columns[i]: column_types[i] for i in range(len(columns))}
+        return Response(dumps({"columns": columns, "data": data}), status=200)
+    except Exception as e:
+        if conn:
+            conn.close()
+        return Response(dumps({"error": str(e)}), status=500)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host="0.0.0.0", debug=True)
